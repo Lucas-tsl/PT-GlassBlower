717dab327fcaf1828f8d49073ee95a4019f0eb4a
Lucas Troteseil <troteseil.lucas@gmail.com>
2024-08-12 15:30:17 +0200

    création header,banner & animations

--- portfolio-starter-main/src/components/Header.js
+++ portfolio-starter-main/src/components/Header.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
+// images
+import Logo from '../assets/logo.png';
+
 const Header = () => {
-  return <div className="bg-pink-300">Header</div>;
+  return (
+      <header className="py-8">
+        <div className="container mx-auto">
+          <div className="flex justify-between items-center">
+            {/* logo*/}
+            <a href="#">
+              <img src={Logo} alt="" className="w-24 h-auto" />
+            </a>
+            {/* button */}
+            <button className="btn btn-sm">work with me</button>
+          </div>
+        </div>
+      </header>
+  );
 };
 
 export default Header;
--- portfolio-starter-main/src/components/Nav.js
+++ portfolio-starter-main/src/components/Nav.js
@@ -1,49 +1,54 @@
 import React from 'react';
-import { BiHomeAlt, BiUser} from "react-icons/bi";
-import {BsClipboardData, BsBriefcase, BsChatSquare} from "react-icons/bs";
-import {Link} from 'react-scroll';
+import { BiHomeAlt, BiUser } from "react-icons/bi";
+import { BsClipboardData, BsBriefcase, BsChatSquare } from "react-icons/bs";
+import { Link } from 'react-scroll';
 
 const Nav = () => {
-  return (
-      <nav className="fixed bottom-2 lg:bottom-0 w-full overflow-hidden z-50">
-          <div className="container mx-auto">
-              {/* nav inner */}
-              <div className="w-full bg-black/20 h-[96px] backdrop-blur-2xl rounded-full max-w-[460px] px-5 flex justify-between items-center text-2xl text-white/50">
-                  <Link to="home"
-                        activeclass={true}
-                        smooth={true}
-                        spy={true} className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
-                      <BiHomeAlt/>
-                  </Link>
-                  <Link to="about"
-                        activeclass={true}
-                        smooth={true}
-                        spy={true} className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
-                      <BiUser/>
-                  </Link>
-                  <Link to="services"
-                        activeclass={true}
-                        smooth={true}
-                        spy={true} className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
-                      <BsClipboardData/>
-                  </Link>
-                  <Link to="work"
-                        activeclass={true}
-                        smooth={true}
-                        spy={true} className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
-                      <BsBriefcase/>
-                  </Link>
-
-                  <Link to="contact"
-                        activeclass={true}
-                        smooth={true}
-                        spy={true} className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
-                      <BsChatSquare/>
-                  </Link>
-              </div>
-          </div>
-      </nav>
-  );
+    return (
+        <nav className="fixed bottom-2 lg:bottom-4 w-full overflow-hidden z-50">
+            <div className="flex justify-center">
+                {/* nav inner */}
+                <div className="bg-black/20 h-[96px] backdrop-blur-2xl rounded-full max-w-[460px] w-full px-5 flex justify-between items-center text-2xl text-white/50">
+                    <Link to="home"
+                          activeclass={true}
+                          smooth={true}
+                          offset={-200}
+                          spy={true}
+                          className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+                        <BiHomeAlt/>
+                    </Link>
+                    <Link to="about"
+                          activeclass={true}
+                          smooth={true}
+                          spy={true}
+                          className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+                        <BiUser/>
+                    </Link>
+                    <Link to="services"
+                          activeclass={true}
+                          smooth={true}
+                          spy={true}
+                          className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+                        <BsClipboardData/>
+                    </Link>
+                    <Link to="work"
+                          activeclass={true}
+                          smooth={true}
+                          spy={true}
+                          className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+                        <BsBriefcase/>
+                    </Link>
+                    <Link to="contact"
+                          activeclass={true}
+                          smooth={true}
+                          spy={true}
+                          className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+                        <BsChatSquare/>
+                    </Link>
+                </div>
+            </div>
+        </nav>
+    );
 };
 
 export default Nav;
